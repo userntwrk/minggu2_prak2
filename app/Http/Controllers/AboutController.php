0b1391cf73375aeb70695c88cf4d46d04deed6ef
wahyu <pratamawahyu090@gmail.com>
2021-09-14 19:43:20 +0700

    praktikum 2

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+  public function index()
+  {
+     return 'NIM : 2031710137<br> Name : Muhammad Roofiif Wahyu Pratama';
+  }
+}
